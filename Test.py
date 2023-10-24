917ca2b88e8f8706088512930cc4d0efcdd7973a
Nitish Raj Moganti (nmoganti) <nmoganti@cisco.com>
2023-10-24 21:39:33 +0530

    Added python file

---
+++ Test.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+
+# Set the path to the ChromeDriver executable (change this path to the location of your ChromeDriver)
+driver_path = "path/to/chromedriver"
+
+# Create a Chrome driver instance
+driver = webdriver.Chrome(executable_path=driver_path)
+
+# Navigate to Amazon.com
+driver.get("https://www.amazon.com/")
+
+# Perform additional actions or web automation tasks here
+
+# Close the browser
+driver.quit()
